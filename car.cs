3aa45d7eb87dd177fbb1957ea86f26140dda54c3
hp <hp@LAPTOP-P49JC8OB>
2024-03-25 21:10:16 -0400

    Add project files.

---
+++ car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Car Class
+public class car : Vehicle
+{
+  
+    public int Seats { get; set; }
+    public string EngineType { get; set; }
+    public string Transmission { get; set; }
+    public bool Convertible { get; set; }
+
+
+    public car(string model, string manufacturer, int year, double rentalPrice, int seats, string engineType, string transmission, bool convertible)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        Seats = seats;
+        EngineType = engineType;
+        Transmission = transmission;
+        Convertible = convertible;
+    }
+
+ 
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Seats: {Seats}, Engine Type: {EngineType}, Transmission: {Transmission}, Convertible: {Convertible}");
+    }
+}
---
+++ vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public double RentalPrice { get; set; }
+
+
+    public vehicle(string model, string manufacturer, int year, double rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice:C}");
+    }
+}
